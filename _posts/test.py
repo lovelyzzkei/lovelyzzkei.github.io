750764f761ac00038e9d2cf35ff8f529b35b5de6
lovelyzzkei <march381@naver.com>
2021-04-26 17:06:47 +0900

    updates

--- _posts/test.py
+++ _posts/test.py
@@ -1,16 +1,5 @@
-import tensorflow as tf
-import numpy as np
-
-t1 = tf.constant(3.0, tf.float32)
-t2 = tf.constant(4.0, tf.float32)
-
-@tf.function
-def _add(t1, t2):
-    return t1 + t2
-
-t3 = _add(t1, t2)
-print(t1)
-print(t2)
-print(t3)
-
-print(t1.numpy())
+for x in range(1, 11):
+    for y in range(1, 11):
+        if 4*x + 5*y == 60:
+            print(x, y)
+            
